051488ba237a383197284d837a3a5d2de10c1f80
Blumenzupfer <sebastianheyde@t-online.de>
2023-11-22 18:59:07 +0100

    tests commit to pull data

--- WpfMauiLibrary/Services/DataAccessService.cs
+++ WpfMauiLibrary/Services/DataAccessService.cs
@@ -1,7 +1,24 @@
 ﻿
+using SQLite;
+
 namespace WpfMauiLibrary.Services
 {
-    internal class DataAccessService
+    public class DataAccessService
     {
+        public string DbPath { get; set; }
+        private SQLiteConnection _connection;
+
+        public DataAccessService(string dbPath)
+        {
+            DbPath = dbPath;
+            _connection = new(dbPath);
+        }
+
+        public List<T> GetAll<T>()
+        {
+            _connection.Query<T>()
+        }
+
+
     }
 }
