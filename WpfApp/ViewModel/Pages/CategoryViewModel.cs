85e91704f2a824b79349bfba3d6be7878f3228d7
Sebastian H <sebastianheyde@t-online.de>
2024-01-04 10:38:14 +0100

    Implemented reverse sorting

--- WpfApp/ViewModel/Pages/CategoryViewModel.cs
+++ WpfApp/ViewModel/Pages/CategoryViewModel.cs
@@ -4,14 +4,14 @@ using WpfMauiLibrary.Models;
 using WpfMauiLibrary.Services;
 using WpfMauiLibrary.HelperClasses;
 using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace WpfApp.ViewModel.Pages;
 
-public partial class CategoryViewModel : ObservableObject
-{
+public partial class CategoryViewModel : ObservableObject {
     [ObservableProperty]
     private string _pageTitle;
-    
+
     // New Task Creation
     [ObservableProperty] private string _taskContent;
     [ObservableProperty] private DateTime? _taskDueDate;
@@ -41,23 +41,25 @@ public partial class CategoryViewModel : ObservableObject
             }
         }
     }
+    [ObservableProperty] private string _sortingDirection;
+    [ObservableProperty] private string _reverseButtonText;
+    [ObservableProperty] private Visibility _reverseButtonVisibility = Visibility.Hidden;
+
+
     // Intern Page Control
     private CategoryModel _categoryModel;
     private ToDoTaskDataAccess _taskDb = new(Constants.DbFullPathWpf);
 
-    public CategoryViewModel(CategoryModel categoryModel)
-    {
+    public CategoryViewModel(CategoryModel categoryModel) {
         _categoryModel = categoryModel;
         PageTitle = _categoryModel.Name;
         LoadTasksFromDb();
-        
+
     }
 
     [RelayCommand]
-    private void CreateNewTask()
-    {
-        if (ValidateTaskContent())
-        {
+    private void CreateNewTask() {
+        if (ValidateTaskContent()) {
             ToDoTaskModel newTask = new(TaskContent, TaskDueDate, TaskPriority, _categoryModel.Id);
             _taskDb.InsertOne(newTask);
             newTask.Id = _taskDb.GetLastImplementedId();
@@ -67,18 +69,13 @@ public partial class CategoryViewModel : ObservableObject
     }
 
     [RelayCommand]
-    private void TaskStatusChanged(object obj)
-    {
-        if(obj is ToDoTaskModel toDoTask)
-        {
-            if(toDoTask.IsCompleted == true)
-            {
+    private void TaskStatusChanged(object obj) {
+        if (obj is ToDoTaskModel toDoTask) {
+            if (toDoTask.IsCompleted == true) {
                 toDoTask.IsCompleted = false;
                 CompletedTasks.Remove(toDoTask);
                 OpenTasks.Add(toDoTask);
-            } 
-            else
-            {
+            } else {
                 toDoTask.IsCompleted = true;
                 OpenTasks.Remove(toDoTask);
                 CompletedTasks.Add(toDoTask);
@@ -88,9 +85,8 @@ public partial class CategoryViewModel : ObservableObject
     }
 
     [RelayCommand]
-    private void DeleteTask()
-    {
-        if(SelectedTask != null) {
+    private void DeleteTask() {
+        if (SelectedTask != null) {
             _taskDb.DeleteOne(SelectedTask.Id);
             if (OpenTasks.Contains(SelectedTask)) {
                 OpenTasks.Remove(SelectedTask);
@@ -100,16 +96,51 @@ public partial class CategoryViewModel : ObservableObject
         }
     }
 
+    [RelayCommand]
+    private void ReverseSorting() {
+        if(SortingDirection == "Descending") {
+            SortingOptionChangedReverse();
+        } else {
+            SortingOptionChanged();
+        }
+    }
+
     private void SortingOptionChanged() {
+        ReverseButtonVisibility = Visibility.Visible;
         switch (SelectedSortingOption) {
             case "Alphabetical":
                 OpenTasks = new ObservableCollection<ToDoTaskModel>(OpenTasks.OrderBy(x => x.Content));
                 CompletedTasks = new ObservableCollection<ToDoTaskModel>(CompletedTasks.OrderBy(x => x.Content));
+                ReverseButtonText = "Alphabetical ↓";
                 break;
             case "Importance":
                 break;
             case "Due Date":
                 break;
+        }
+        SortingDirection = "Descending";
+    }
+
+    private void SortingOptionChangedReverse() {
+        switch (SelectedSortingOption) {
+            case "Alphabetical":
+                OpenTasks = new ObservableCollection<ToDoTaskModel>(OpenTasks.Reverse());
+                CompletedTasks = new ObservableCollection<ToDoTaskModel>(CompletedTasks.Reverse());
+                ReverseButtonText = "Alphabetical ↑";
+                break;
+            case "Importance":
+                break;
+            case "Due Date":
+                break;
+        }
+        SortingDirection = "Reverse";
+    }
+
+    private void ChangeSortingDirection() {
+        if(SortingDirection == "Descending") {
+            SortingDirection = "Reverse";
+        } else {
+            SortingDirection = "Descending";
         }
     }
 
